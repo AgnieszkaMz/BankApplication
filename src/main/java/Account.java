bea0ba97cce990f35aa0a6db84328d33987ac67e
AgMz <agmz@costam.pl>
2022-04-26 18:54:04 +0200

    add new method: create account

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 public class Account {
     private String AccountName;
     private String AccountSurname;
     private String AccountNumber;
     private long balance;
+
+    Scanner scanner = new Scanner(System.in);
 
     public String getAccountName(){
         return AccountName;
@@ -21,5 +24,22 @@ public class Account {
     }
     public void setAccountNumber(String AccountNumber) {
         this.AccountNumber = AccountNumber;
+    }
+    public long getBalance() {
+        return balance;
+    }
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+    public void createAccount() {
+        System.out.println("Enter your name");
+        AccountName = scanner.next();
+        System.out.println("Enter your surname");
+        AccountSurname = scanner.next();
+        System.out.println("Enter your account number");
+        AccountNumber = scanner.next();
+        System.out.println("Enter your balance");
+        balance = scanner.nextLong();
+
     }
 }
