d3c2f37ba2047d44a88b24685c549edcf7259826
AgMz <agmz@costam.pl>
2022-04-26 18:30:40 +0200

    add getters and setters for AccountSurname and AccountNumber param

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -4,11 +4,22 @@ public class Account {
     private String AccountNumber;
     private long balance;
 
-    public String getAcountName(){
+    public String getAccountName(){
         return AccountName;
     }
     public void setAccountName(String AccountName){
         this.AccountName = AccountName;
     }
-
+    public String getAccountSurname() {
+        return AccountSurname;
+    }
+    public void setAccountSurname(String AccountSurname) {
+        this.AccountSurname = AccountSurname;
+    }
+    public String getAccountNumber() {
+        return AccountNumber;
+    }
+    public void setAccountNumber(String AccountNumber) {
+        this.AccountNumber = AccountNumber;
+    }
 }
