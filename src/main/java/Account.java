e00fc64d5380b2cf76f4db7246af572ab534c559
AgMz <agmz@costam.pl>
2022-04-26 18:11:12 +0200

    add BankApp class and set for parameters and getter and setter for AccountName param

---
+++ src/main/java/Account.java
@@ -0,0 +1,14 @@
+public class Account {
+    private String AccountName;
+    private String AccountSurname;
+    private String AccountNumber;
+    private long balance;
+
+    public String getAcountName(){
+        return AccountName;
+    }
+    public void setAccountName(String AccountName){
+        this.AccountName = AccountName;
+    }
+
+}
